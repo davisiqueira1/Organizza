0c07a8e7ac621e7145f636f9229bef32cb9dc7f5
Davi Siqueira <davisvsiqueira@gmail.com>
2025-02-02 19:02:46 -0300

    chore: add project structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+const app = express();
+
+// const eventRoutes = require('./routes/events');
+// const userRoutes = require('./routes/usuarios');
+// const ticketRoutes = require('./routes/ingressos');
+// const couponRoutes = require('./routes/cupons');
+
+app.use(express.json());
+
+// app.use(eventRoutes);
+// app.use(userRoutes);
+// app.use(ticketRoutes);
+// app.use(couponRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
